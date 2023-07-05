061860160dc83db34fdefb398bcdddc4f1f01bef
luissantos16 <lmsr.1601@gmail.com>
2023-07-05 10:16:04 -0500

    header responsive

---
+++ js/java.js
@@ -0,0 +1,6 @@
+function llamar(){
+    let menudespegable=document.getElementById("listaid");
+        menudespegable.classList.toggle("lista-open")
+    let listbar=document.getElementById("lista-bar");
+    listbar.querySelector("lista-bar .listabar").classList.toggle;
+}
